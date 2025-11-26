e527c60cbb48a6911278b9d848c4d982aa101a93
Giant Pink Robots! <giantpinkrobots@protonmail.com>
2025-11-26 15:10:50 +0100

    Don't compile dependencies

--- makerelease-osx-v2.mk
+++ makerelease-osx-v2.mk
@@ -93,60 +93,6 @@ export LDFLAGS
 
 LTO_FLAGS = -flto -ffunction-sections -fdata-sections
 
-# Dependency versions
-zlib_version = 1.2.11
-zlib_hash = c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
-zlib_url = http://zlib.net/fossils/zlib-$(zlib_version).tar.gz
-
-expat_version = 2.2.8
-expat_hash = bd507cba42716ca9afe46dd3687fb0d46c09347517beb9770f53a435d2c67ea0
-expat_url = https://github.com/libexpat/libexpat/releases/download/R_2_2_8/expat-2.2.8.tar.gz
-expat_cflags=$(CFLAGS) $(LTO_FLAGS)
-expat_ldflags=$(CFLAGS) $(LTO_FLAGS)
-
-cares_version = 1.15.0
-cares_hash = 6cdb97871f2930530c97deb7cf5c8fa4be5a0b02c7cea6e7c7667672a39d6852
-cares_url = https://github.com/c-ares/c-ares/releases/download/cares-1_15_0/c-ares-$(cares_version).tar.gz
-cares_confflags = "--enable-optimize=$(OPTFLAGS)"
-cares_cflags=$(CFLAGS) $(LTO_FLAGS)
-cares_ldflags=$(CFLAGS) $(LTO_FLAGS)
-
-sqlite_version = autoconf-3300000
-sqlite_hash = e0a8cf4c7a87455e55e10413d16f358ca121ccec687fe1301eac95e2d340fc58
-sqlite_url = https://sqlite.org/2019/sqlite-$(sqlite_version).tar.gz
-sqlite_cflags=$(CFLAGS) $(LTO_FLAGS)
-sqlite_ldflags=$(CFLAGS) $(LTO_FLAGS)
-
-gmp_version = 6.3.0
-gmp_hash = ac28211a7cfb609bae2e2c8d6058d66c8fe96434f740cf6fe2e47b000d1c20cb
-gmp_url = https://ftp.gnu.org/gnu/gmp/gmp-$(gmp_version).tar.bz2
-gmp_confflags = --disable-cxx --enable-assembly --with-pic --enable-fat
-gmp_cflags=$(CFLAGS)
-gmp_cxxflags=$(CXXFLAGS)
-
-libgpgerror_version = 1.36
-libgpgerror_hash = babd98437208c163175c29453f8681094bcaf92968a15cafb1a276076b33c97c
-libgpgerror_url = https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-$(libgpgerror_version).tar.bz2
-libgpgerror_cflags=$(CFLAGS) $(LTO_FLAGS)
-libgpgerror_ldflags=$(CFLAGS) $(LTO_FLAGS)
-libgpgerror_confflags = --with-pic --disable-languages --disable-doc --disable-nls
-
-libgcrypt_version = 1.8.5
-libgcrypt_hash = 3b4a2a94cb637eff5bdebbcaf46f4d95c4f25206f459809339cdada0eb577ac3
-libgcrypt_url = https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-$(libgcrypt_version).tar.bz2
-libgcrypt_confflags=--with-gpg-error-prefix=$(PWD)/arch --disable-O-flag-munging --disable-asm --disable-amd64-as-feature-detection
-libgcrypt_cflags=$(PLATFORMFLAGS)
-libgcrypt_cxxflags=$(PLATFORMFLAGS)
-
-libssh2_version = 1.9.0
-libssh2_hash = d5fb8bd563305fd1074dda90bd053fb2d29fc4bce048d182f96eaa466dfadafd
-libssh2_url = https://www.libssh2.org/download/libssh2-$(libssh2_version).tar.gz
-libssh2_cflags=$(CFLAGS) $(LTO_FLAGS)
-libssh2_cxxflags=$(CXXFLAGS) $(LTO_FLAGS)
-libssh2_ldflags=$(CFLAGS) $(LTO_FLAGS)
-libssh2_confflags = --with-pic --without-openssl --with-libgcrypt=$(PWD)/arch --with-libgcrypt-prefix=$(PWD)/arch
-libssh2_nocheck = yes
-
 
 # ARCHLIBS that can be template build
 ARCHLIBS = expat cares sqlite gmp libgpgerror libgcrypt libssh2
@@ -164,18 +110,20 @@ ARIA2_CONFFLAGS = \
         --enable-bittorrent \
         --disable-nls \
         --with-appletls \
-        --with-libgmp=$(PWD)/arch \
-        --with-sqlite3=$(PWD)/arch \
-        --with-libz=$(PWD)/arch \
-        --with-libexpat=$(PWD)/arch \
-        --with-libcares=$(PWD)/arch \
-        --with-libgcrypt=$(PWD)/arch \
-        --with-libssh2=$(PWD)/arch \
+        --with-libz=/usr/local/opt/zlib \
+		--with-gmp=/usr/local/opt/gmp \
+		--with-libgcrypt=/usr/local/opt/libgcrypt \
+		--with-libssh2=/usr/local/opt/libssh2 \
+		--with-sqlite3=/usr/local/opt/sqlite \
+		--with-expat=/usr/local/opt/expat \
+		--with-libcares=/usr/local/opt/c-ares \
         --without-libuv \
         --without-gnutls \
         --without-openssl \
         --without-libnettle \
         --without-libxml2 \
+		CFLAGS="-I/usr/local/include" \
+  		LDFLAGS="-L/usr/local/lib -static" \
         ARIA2_STATIC=yes
 
 # Detect number of CPUs to be used with make -j
@@ -183,40 +131,6 @@ CPUS = $(shell sysctl hw.ncpu | cut -d" " -f2)
 
 # default target
 all::
-
-# All those .PRECIOUS files, because otherwise gmake will treat them as
-# intermediates and remove them when the build completes. Thanks gmake!
-.PRECIOUS: %.tar.gz
-%.tar.gz:
-	curl -o $@ -A 'curl/0; like wget' -L \
-		$($(basename $(basename $@))_url)
-
-.PRECIOUS: %.check
-%.check: %.tar.gz
-	@if test "$$(shasum -a256 $< | awk '{print $$1}')" != "$($(basename $@)_hash)"; then \
-		echo "Invalid $@ hash"; \
-		rm -f $<; \
-		exit 1; \
-	fi;
-	touch $@
-
-.PRECIOUS: %.stamp
-%.stamp: %.tar.gz %.check
-	tar xf $<
-	mv $(basename $@)-$($(basename $@)_version) $(basename $@)
-	touch $@
-
-.PRECIOUS: cares.stamp
-cares.stamp: cares.tar.gz cares.check
-	tar xf $<
-	mv c-ares-$($(basename $@)_version) $(basename $@)
-	touch $@
-
-.PRECIOUS: libgpgerror.stamp
-libgpgerror.stamp: libgpgerror.tar.gz libgpgerror.check
-	tar xf $<
-	mv libgpg-error-$($(basename $@)_version) $(basename $@)
-	touch $@
 
 # Using (NON)ARCH_template kinda stinks, but real multi-target pattern rules
 # only exist in feverish dreams.
@@ -226,20 +140,6 @@ $(1).build: $(1).$(ARCH).build
 deps:: $(1).build
 
 endef
-
-.PRECIOUS: zlib.%.build
-zlib.%.build: zlib.stamp
-	$(eval BASE := $(basename $<))
-	$(eval DEST := $(basename $@))
-	$(eval ARCH := $(subst .,,$(suffix $(DEST))))
-	rsync -a $(BASE)/ $(DEST)
-	( cd $(DEST) && ./configure \
-		--static --prefix=$(PWD)/arch \
-		)
-	$(MAKE) -C $(DEST) -sj$(CPUS) CFLAGS="$(CFLAGS) $(LTO_FLAGS) -arch $(ARCH)"
-	$(MAKE) -C $(DEST) -sj$(CPUS) CFLAGS="$(CFLAGS) $(LTO_FLAGS) -arch $(ARCH)" check
-	$(MAKE) -C $(DEST) -s install
-	touch $@
 
 $(foreach lib,$(NONARCHLIBS),$(eval $(call NONARCH_template,$(lib))))
 
@@ -272,7 +172,7 @@ endef
 $(foreach lib,$(ARCHLIBS),$(eval $(call ARCH_template,$(lib))))
 
 .PRECIOUS: aria2.%.build
-aria2.%.build: zlib.%.build expat.%.build gmp.%.build cares.%.build sqlite.%.build libgpgerror.%.build libgcrypt.%.build libssh2.%.build
+aria2.%.build:
 	$(eval DEST := $$(basename $$@))
 	$(eval ARCH := $$(subst .,,$$(suffix $$(DEST))))
 	mkdir -p $(DEST)
